6114d3631c18bb8f5f0134e204b2c450f4af49f2
Kieu <bsb20131417@gmail.com>
2023-09-08 18:09:59 +0900

    add gender(enum, validates)

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,9 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
+  validates :gender, presence: true
+
+  enum gender: { others: 0, male: 1, female: 2 }
 
   scope :allowing_created_event_notification,
         -> { joins(:notification_timings).merge(NotificationTiming.created_event) }
